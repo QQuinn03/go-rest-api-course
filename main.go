8b0d60663bb60df42088fa224902e1908de68885
Qiong Qin <zoeqin03@gmail.com>
2022-03-02 23:07:43 -0500

    try

--- main.go
+++ main.go
@@ -1,19 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	transportHTTP "github.com/QQuinn03/go-rest-api-course/internal/tansport/http"
+	//transportHTTP "internal/transport/http"
+)
 
 type App struct{}
 
-func (*App) Run() error {
-	fmt.Println("Setting up our APP")
-	return nil
+func (app *App) Run() error {
+	fmt.Println("Setting Up Our APP")
 
+	handler := transportHTTP.NewHandler()
+	handler.SetupRoutes()
+
+	if err := http.ListenAndServe(":8080", handler.Router); err != nil {
+		fmt.Println("Failed to set up server")
+		return err
+	}
+
+	return nil
 }
+
 func main() {
-	//fmt.Println("h")
+	fmt.Println("Go REST API Course")
 	app := App{}
 	if err := app.Run(); err != nil {
-		fmt.Println("Error starting up our rest api")
+		fmt.Println("Error starting up our REST API")
 		fmt.Println(err)
 	}
 }
