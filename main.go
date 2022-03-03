b842eca4dabc124b4cc478977bfa0755bfde1a41
Qiong Qin <zoeqin03@gmail.com>
2022-03-02 22:21:24 -0500

    structuring our application

--- main.go
+++ main.go
@@ -2,6 +2,18 @@ package main
 
 import "fmt"
 
+type App struct{}
+
+func (*App) Run() error {
+	fmt.Println("Setting up our APP")
+	return nil
+
+}
 func main() {
-	fmt.Println("h")
+	//fmt.Println("h")
+	app := App{}
+	if err := app.Run(); err != nil {
+		fmt.Println("Error starting up our rest api")
+		fmt.Println(err)
+	}
 }
